b17f535c810a885ddfdd67b8ef363e3af107706f
AGRTECH <62949445+AGRTECH@users.noreply.github.com>
2022-12-14 18:00:37 -0500

    sdfg

--- truffle-config.js
+++ truffle-config.js
@@ -2,10 +2,7 @@ require("babel-register");
 require("babel-polyfill");
 require("dotenv").config();
 const HDWalletProvider = require("truffle-hdwallet-provider-privkey");
-const privateKeys = [
-  "5c47a6e890c045a1703825c55457694d8b800cdd496d55337dfde6f7bb2a4cc3",
-  "66e4d1f22c39e1383b79caf7a9c82451dedfee5210e72c27adf40a9ea4aff665",
-];
+const privateKeys = process.env.PRIVATE_KEYS || "";
 
 module.exports = {
   networks: {
@@ -17,8 +14,8 @@ module.exports = {
     goerli: {
       provider: function () {
         return new HDWalletProvider(
-          privateKeys, // Array of account private keys from .env
-          `https://goerli.infura.io/v3/6513d419248741bb86712fca973c1dfb` // Url to an Ethereum Node
+          privateKeys.split(","), // Array of account private keys from .env
+          `https://goerli.infura.io/v3/${process.env.INFURA_API_KEY}` // Url to an Ethereum Node
         );
       },
       gas: 5000000,
@@ -30,6 +27,7 @@ module.exports = {
   contracts_build_directory: "./src/abis/",
   compilers: {
     solc: {
+      version: "0.8.17",
       optimizer: {
         enabled: true,
         runs: 200,

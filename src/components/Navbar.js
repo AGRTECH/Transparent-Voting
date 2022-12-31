cf8524a899bcef2874de29ad51eba16354ebb91b
AGRTECH <62949445+AGRTECH@users.noreply.github.com>
2022-12-31 15:33:20 -0500

    nav account revamp

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React, { Component, useState, useEffect } from "react";
 import { connect } from "react-redux";
+import wallet from "../img/biggerwallet1.png";
 import "./App.css";
 import {
   accountSelector,
@@ -40,23 +41,40 @@ const Navbar = (props) => {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark  navflex">
       <img className="logo-img" src={logo} alt="" />
-      {props.accountLoaded ? (
-        <a className=" nav-account">
-          {props.votingLoaded
-            ? `Account: ${props.account
-                .split("")
-                .splice(0, 5, "")
-                .join("")}...${props.account
-                .split("")
-                .splice(38, 4, "")
-                .join("")}`
-            : window.ethereum.networkVersion !== 5 && account.length > 1
-            ? "Please Switch to Goerli"
-            : "Connect Wallet"}
-        </a>
-      ) : (
-        <p>Please Connect Your Wallet</p>
-      )}
+      <div>
+        <img
+          src={wallet}
+          style={{
+            width: "55px",
+            height: "50px",
+            border: "3px solid black",
+            borderRadius: "50%",
+            zIndex: "100",
+            marginRight: "-32px",
+            marginBottom: "3px",
+            position: "relative",
+            backgroundColor: "#905ece",
+          }}
+          alt=""
+        />
+        {props.accountLoaded ? (
+          <a className=" nav-account">
+            {props.votingLoaded
+              ? `Account: ${props.account
+                  .split("")
+                  .splice(0, 2, "")
+                  .join("")}...${props.account
+                  .split("")
+                  .splice(38, 4, "")
+                  .join("")}`
+              : window.ethereum.networkVersion !== 5 && account.length > 1
+              ? "Please Switch to Goerli"
+              : "Connect Wallet"}
+          </a>
+        ) : (
+          <p>Please Connect Your Wallet</p>
+        )}
+      </div>
     </nav>
   );
 };
